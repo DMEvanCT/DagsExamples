8e9f59bc607ffb446c14e58c5568155ac967b8ad
Evan Haston <evan.haston@slalom.com>
2022-02-12 21:40:53 -0500

    fix: DAG fixes

--- dags/hello_word.py
+++ dags/hello_word.py
@@ -1,11 +1,11 @@
-from airflow import DAG
+from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 def helloWorld():
     print("Hello World")
 
-with DAG(dag_id="hello_world_dag"
+with DAG(dag_id="hello_world_dag",
     start_date=datetime(2022, 1, 1),
     schedule_interval="@hourly",
     catchup=False) as dag:
