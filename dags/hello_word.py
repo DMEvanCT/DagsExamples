5b5014dfce81732064276648be211e2f08ce9d72
Evan Haston <evan.haston@slalom.com>
2022-02-12 21:29:30 -0500

    fix: DAG fixes

--- dags/hello_word.py
+++ dags/hello_word.py
@@ -6,13 +6,13 @@ def helloWorld():
     print("Hello World")
 
 with DAG(dag_id="hello_world_dag"
-start_date=datetime(2022, 1, 12),
-schedule_interval="@hourly",
-catchup=False) as dag:
+    start_date=datetime(2022, 1, 1),
+    schedule_interval="@hourly",
+    catchup=False) as dag:
 
-task1 = PythonOperator(
+    task1 = PythonOperator(
     task_id="hello_word"
     python_callable=hello_word)
-)
+    )
 
 task1
